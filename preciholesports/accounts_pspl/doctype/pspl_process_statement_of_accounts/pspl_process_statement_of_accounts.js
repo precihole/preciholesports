319c03d43808924b67e17b55b2697c50013fe4c0
rehanansari389 <rehan@preciholesports.com>
2023-03-12 12:17:30 +0530

    accounts module

---
+++ preciholesports/accounts_pspl/doctype/pspl_process_statement_of_accounts/pspl_process_statement_of_accounts.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, preciholesports and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PSPL Process Statement Of Accounts', {
+	// refresh: function(frm) {
+
+	// }
+});
